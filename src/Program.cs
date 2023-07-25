db642d8e2a0449940b02fdedb4517e19e7b27db6
Priya <itsmepriyap@gmail.com>
2023-07-26 09:38:23 +1000

    Add project files.

---
+++ src/Program.cs
@@ -0,0 +1,27 @@
+﻿using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CampContext>();
+builder.Services.AddScoped<ICampRepository, CampRepository>();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+  app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
